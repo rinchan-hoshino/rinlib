676b59fc81ea5540cb9686280484def6b900ac40
rinchanai <275440557+rinchanai@users.noreply.github.com>
2026-08-24 23:10:25 +0800

    feat: add shared state API for Minecraft 1.21.11

---
+++ fabric/src/main/java/dev/rinchan/rinlib/fabric/RinLibFabric.java
@@ -0,0 +1,9 @@
+package dev.rinchan.rinlib.fabric;
+
+import net.fabricmc.api.ModInitializer;
+
+public final class RinLibFabric implements ModInitializer {
+    @Override
+    public void onInitialize() {
+    }
+}
